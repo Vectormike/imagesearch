7bd49207680becb862007c12d818f466db325f5b
Vectormike40 <victor.junior40@yahoo.com>
2019-04-23 10:36:50 +0100

    Created Search bar

---
+++ src/components/SearchField/SearchField.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SearchField() {
+
+	return(
+		<div>
+			<input 
+				type='Search' 
+				placeholder='Search for that beautiful photo'
+			/>
+		</div>
+	);
+}
+
+export default SearchField;

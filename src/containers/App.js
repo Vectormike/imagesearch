6967cdf7533fde209bb89987d34716d3b849a00c
Vectormike40 <victor.junior40@yahoo.com>
2019-04-23 16:31:34 +0100

    Shit!

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,6 @@ import SearchBar from '../components/SearchField/SearchField'
 const apiKey = '581eb4a49e4ae7142198627eae4a64b445a6bd9af7bb96de5a3cf51e2b62d919';
 function App() {
 
-  const [searchField, setSearchField] = useState('');
   const [photos, setPhotos] = useState([]);
 
   // getPhotos = async (photo) => {
@@ -19,28 +18,24 @@ function App() {
   // }
 
   useEffect(() => {
-      let fetchData = async (query) => {
-      try {
-      let data = await fetch(`https://api.unsplash.com/search/photos?client_id=${apiKey}&per_page=2&query=${query}`);
-      const items = await data.json();
-      setPhotos(items.results);
-      } catch (error) {
-        console.log(`Error is ${error}`)
-      }
-    }
     fetchData();
-    
-    console.log(photos);
-  }, [searchField]);
+  });
   
-  function onSearchChange(e) {
-    setSearchField(e.target.value)
-    this.fetchData(searchField)
+
+  async function fetchData(){
+    try {
+    let data = await fetch(`https://api.unsplash.com/search/photos?client_id=${apiKey}&per_page=2&query=${term}`);
+    const items = await data.json();
+    setPhotos(items.results);
+    } catch (error) {
+      console.log(`Error is ${error}`)
+    }
   }
+
 
   return (
     <div>
-      <SearchBar onSearch={onSearchChange} search={searchField}/>
+      <SearchBar/>
     </div>
   );
 }

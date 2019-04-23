405ba65290ae71decc835816ef416d39eaab4c82
Vectormike40 <victor.junior40@yahoo.com>
2019-04-23 16:04:59 +0100

    Using Hooks in this component.

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -2,12 +2,22 @@ import React from "react";
 
 function SearchField() {
 
+	const [term, setTerm] = useState('');
+
+	// Function to setTerm when user inputs
+	function onInput(e){
+		setTerm(e.target.value)
+		console.log(term)
+	}
+  
 	return(
 		<div>
 			<input 
-				type='Search' 
+				type='Search'
 				placeholder='Search for that beautiful photo'
+				onChange={onInput}
 			/>
+			<button type="submit">Click</button>
 		</div>
 	);
 }

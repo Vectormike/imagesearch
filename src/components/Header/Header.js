0c5438cecb56f71e40995603c46f464ce46e45f6
Vectormike40 <victor.junior40@yahoo.com>
2019-04-24 11:53:54 +0100

    Created Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+        <div className="App">
+            <h1>Image Search App</h1>
+        </div>
+    );
+}
+
+export default Header;

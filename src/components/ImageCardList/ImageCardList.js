8e75b0c5e174436bfc5d1e2866158760885877d4
Vectormike40 <victor.junior40@yahoo.com>
2019-04-24 11:54:40 +0100

    Usethis to loop all images.

---
+++ src/components/ImageCardList/ImageCardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ImageCard from '../ImageCard/ImageCard';
+
+
+function ImageCardList(){
+    const {} = data;
+    return(
+        <div>
+            {
+                
+            }
+        </div>
+    );
+
+}
+
+export default ImageCardList;

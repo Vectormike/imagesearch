1a452e041431351679732c5e7083ac2481c7297b
Vectormike40 <victor.junior40@yahoo.com>
2019-04-24 11:54:23 +0100

    Created Image card or whatever.

---
+++ src/components/ImageCard/ImageCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ImageCard(){
+
+    return (
+        <div>
+            <div>
+                <span><img src={} alt='' title={}/></span>
+                <p>`Image by ${}`</p>
+            </div>
+        </div>
+    );
+}
+
+export default ImageCard;

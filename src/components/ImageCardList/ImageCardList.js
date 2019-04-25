247526ec1b1f0c309f739d444378dcc452447cb8
Vectormike40 <victor.junior40@yahoo.com>
2019-04-25 09:58:20 +0100

    Bug here..

--- src/components/ImageCardList/ImageCardList.js
+++ src/components/ImageCardList/ImageCardList.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ImageCard from '../ImageCard/ImageCard';
 
 
-function ImageCardList(){
-    const {} = data;
-    return(
-        <div>
-            {
-                
-            }
-        </div>
-    );
+function ImageCardList({data}){
+	// const {alt_description, urls, likes, user} = data;
+	return(
+		<div>
+			{
+				data.map((res, i) => {
+					return(
+						<ImageCard
+							key={i}
+							description={res.alt_description}
+							likes={res.likes}
+							user={res.user}
+							url={res.url}
+						/>
+					);
+				})
+			}
+		</div>
+	);
 
 }
 

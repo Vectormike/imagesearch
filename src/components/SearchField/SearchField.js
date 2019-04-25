5c2ef359d3a676988dd73628c1ad3535d79d352f
Vectormike40 <victor.junior40@yahoo.com>
2019-04-25 09:58:42 +0100

    Cleanup

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -2,26 +2,17 @@ import React from "react";
 
 function SearchField({onInput, onButtonSubmit}) {
 
-	
-
-	
-	function onFormSubmit(e) {
-		e.preventDefault();
-	}
-
 	return(
 		<div>
-			<form onSubmit={onFormSubmit}>
-				<input 
-					type='Search'
-					placeholder='Search for that beautiful photo'
-					onChange={onInput}
-				/>
-				<button 
-					onClick={onButtonSubmit} 
-					type="submit">Click
-				</button>
-			</form>
+			<input
+				type='Search'
+				placeholder='Search for that beautiful photo'
+				onChange={onInput}
+			/>
+			<button
+				onClick={onButtonSubmit}
+				type="submit">Click
+			</button>
 		</div>
 	);
 }

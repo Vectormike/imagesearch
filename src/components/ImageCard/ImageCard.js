f07ff5c25fddeb29f2b75300b8db071f8802a032
Vectormike40 <victor.junior40@yahoo.com>
2019-04-29 16:23:02 +0100

    Cleanup

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -2,38 +2,20 @@ import React from 'react';
 import './ImageCard.css';
 
 function ImageCard({description, likes, user, url, location}){
-
     return (
-		<div className="container-fluid">
-			<div className="row">
-            <div className="col-sm-6">
-                <img src={url} alt='' title='' className="w-100"/>
-                <dl className="mt2 f6 lh-copy">
+        <div className="flex-container">
+            <div className="flex-item">
+                <img src={url} alt='' title='' className="shadow-3"/>
+                <dl className="f7 pa2 ma br3 bg-near-white">
                     <dt className="clip">Image</dt>        
-                    <dd className="ml0 black truncate w-100">Image from {user}</dd>
+                    <dd className="ml0 black w-100">Photo by {user}</dd>
                     <dt className="clip">Description</dt>
-                    <dd className="ml0 black truncate w-100">Description: {description}</dd>
+                    <dd className="ml0 black w-100">{description}</dd>
                     <dt className="clip">Likes</dt>
-                    <dd className="ml0 black truncate w-100">{likes} likes</dd>
-                    <dt className="clip">Location</dt>
-                    <dd className="ml0 black truncate w-100">:{location}</dd>
-                </dl>
-            </div>
-				<div className="col-sm-6">
-                <img src={url} alt='' title='' className="w-100"/>
-                <dl className="mt2 f6 lh-copy">
-                    <dt className="clip">Image</dt>        
-                    <dd className="ml0 black truncate w-100">Image from {user}</dd>
-                    <dt className="clip">Description</dt>
-                    <dd className="ml0 black truncate w-100">Description: {description}</dd>
-                    <dt className="clip">Likes</dt>
-                    <dd className="ml0 black truncate w-100">{likes} likes</dd>
-                    <dt className="clip">Location</dt>
-                    <dd className="ml0 black truncate w-100">:{location}</dd>
+                    <dd className="ml0 black w-100">{likes} likes</dd>
                 </dl>
             </div>
         </div>
-		</div>
     );
 }
 

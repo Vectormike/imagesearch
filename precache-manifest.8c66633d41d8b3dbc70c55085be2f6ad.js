5ffa149fac0d596c310224c9d62565943b34841c
Vectormike40 <victor.junior40@yahoo.com>
2019-04-30 16:49:22 +0100

    Updates

---
+++ precache-manifest.8c66633d41d8b3dbc70c55085be2f6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f29a7aece3991c7e9a19ee7e1cfee76",
+    "url": "/Vectormike40/imagesearch/index.html"
+  },
+  {
+    "revision": "bc65c2ccb1dbf816f3ec",
+    "url": "/Vectormike40/imagesearch/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3b789a2f59ce60fb248a",
+    "url": "/Vectormike40/imagesearch/static/css/main.7aaeff70.chunk.css"
+  },
+  {
+    "revision": "bc65c2ccb1dbf816f3ec",
+    "url": "/Vectormike40/imagesearch/static/js/2.495f230f.chunk.js"
+  },
+  {
+    "revision": "3b789a2f59ce60fb248a",
+    "url": "/Vectormike40/imagesearch/static/js/main.143e852d.chunk.js"
+  },
+  {
+    "revision": "39f2c26ef7199368935b",
+    "url": "/Vectormike40/imagesearch/static/js/runtime~main.e6862b4d.js"
+  }
+]);

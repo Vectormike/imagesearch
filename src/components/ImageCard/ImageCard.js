8cca96610686db9585d1ee875d59ecaebc576140
Vectormike40 <victor.junior40@yahoo.com>
2019-04-30 12:45:51 +0100

    Cleanup

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -3,9 +3,9 @@ import './ImageCard.css';
 
 function ImageCard({description, likes, user, url, location}){
     return (
-        <div className="">
+        <div className="pa3 ma">
                 <img src={url} alt='' title='' className="shadow-3"/>
-                <dl className="f7 pa2 ma br3 bg-near-white">
+                <dl className="f7 pa2 ma br3 ">
                     <dt className="clip">Image</dt>        
                     <dd className="ml0 black w-100">Photo by {user}</dd>
                     <dt className="clip">Description</dt>
@@ -13,7 +13,7 @@ function ImageCard({description, likes, user, url, location}){
                     <dt className="clip">Likes</dt>
                     <dd className="ml0 black w-100">{likes} <i className="fas fa-heart"></i> </dd>
                 </dl>
-                </div>
+        </div>
     );
 }
 
